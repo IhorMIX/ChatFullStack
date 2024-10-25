a1eee8bd7d35117a731d3a65ff11497947cb3afa
Ігор Колобилін <igor.kolobulin@gmail.com>
2024-10-25 23:08:38 +0300

    create proj

---
+++ Chat/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+using Chat.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Chat.Hubs;
+
+public class ChatHub : Hub
+{
+    public async Task JoinChat(UserConnection connection)
+    {
+        
+    }
+}
---
+++ Chat/Models/UserConnection.cs
@@ -0,0 +1,3 @@
+namespace Chat.Models;
+
+public record UserConnection(string username, string charRoom);
